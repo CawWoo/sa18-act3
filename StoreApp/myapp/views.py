9c1e662d2fca44f84b8284cb02ba1277aac8cf7c
CawWoo <melodicraven@gmail.com>
2024-04-21 21:43:11 -0500

    Added index and show html files

--- StoreApp/myapp/views.py
+++ StoreApp/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+from django.shortcuts import get_object_or_404
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'show.html', {'product': product})
